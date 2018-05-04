0d34e16e6226dbc37b2dfb44937037e150dab110
MelynaPernia <mebipeos@outlook.com>
2018-05-04 17:32:39 -0500

    Estructura del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { ToDoComponent } from './to-do/to-do.component';
+import { NavVerticalComponent } from './nav-vertical/nav-vertical.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    ToDoComponent,
+    NavVerticalComponent
   ],
   imports: [
     BrowserModule
